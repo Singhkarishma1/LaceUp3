b854415df66f1b7fd5997b80df882bcf1762de3a
Singhkarishma1 <145593589+Singhkarishma1@users.noreply.github.com>
2024-11-02 17:21:26 +0530

    alert removed

--- script.js
+++ script.js
@@ -60,7 +60,6 @@ function addToCart(productName, productPrice) {
 
     // Save cart to local storage
     localStorage.setItem('cart', JSON.stringify(cart));
-    alert(`${productName} has been added to your cart.`);
     updateCartCount();
 }
 
